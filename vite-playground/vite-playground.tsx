26e5cf45866427e112c6289feb949b5e33619897
Caleb Eby <caleb.eby01@gmail.com>
2023-03-20 14:13:09 -0700

    WIP proj 3 obj loading

--- vite-playground/vite-playground.tsx
+++ vite-playground/vite-playground.tsx
@@ -1,5 +1,5 @@
 import { render } from "preact";
-import * as p from "../src/pages/proj-2/app";
+import * as p from "../src/pages/proj-3/app";
 import "../src/content-page.css";
 
 const root = document.querySelector("#app")!;
