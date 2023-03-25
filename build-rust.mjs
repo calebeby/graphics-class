0c2c15620c96f794b229b861d0ffd5221871540e
Caleb Eby <caleb.eby01@gmail.com>
2023-03-24 23:31:33 -0700

    Improve build/lint scripts

--- build-rust.mjs
+++ build-rust.mjs
@@ -1,34 +1,42 @@
 import { watch, run } from "watchlist";
 
+const is_dev = process.argv.includes("--dev");
+
 async function build() {
   console.log("running build..");
   await run(
-    "wasm-pack build --target web src/projects/proj-3 --dev --weak-refs",
+    `wasm-pack build --target web src/projects/proj-3 ${
+      is_dev ? "--dev" : "--release"
+    } --weak-refs`,
   );
 }
 
-let queued_build = false;
+if (is_dev) {
+  let queued_build = false;
 
-const handle_change = () => {
-  if (queued_build) return;
-  setTimeout(() => {
-    queued_build = false;
-    build();
-  }, 1);
-  queued_build = true;
-};
+  const handle_change = () => {
+    if (queued_build) return;
+    setTimeout(() => {
+      queued_build = false;
+      build();
+    }, 1);
+    queued_build = true;
+  };
 
-await watch(["src"], handle_change, {
-  ignore: [
-    "node_modules",
-    "package.json",
-    ".gitignore",
-    /\.ts$/,
-    /\.js$/,
-    /\.wasm$/,
-    "target",
-    "Session.vim",
-  ],
-  clear: true,
-  eager: true,
-});
+  await watch(["src"], handle_change, {
+    ignore: [
+      "node_modules",
+      "package.json",
+      ".gitignore",
+      /\.ts$/,
+      /\.js$/,
+      /\.wasm$/,
+      "target",
+      "Session.vim",
+    ],
+    clear: true,
+    eager: true,
+  });
+} else {
+  await build();
+}
