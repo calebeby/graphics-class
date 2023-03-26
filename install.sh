9e9028f0b0dd7072a96106325487dedbd2f4421a
Caleb Eby <caleb.eby01@gmail.com>
2023-03-25 21:45:11 -0700

    try another thing

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-cargo metadata
+# cargo metadata
+which wasm-pack
 wasm-pack --help
 pnpm install
