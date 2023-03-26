50a81647e1a9351dd1dcce3cbbaa2315cf5df0b4
Caleb Eby <caleb.eby01@gmail.com>
2023-03-25 21:53:05 -0700

    Debug

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 rustup default stable
+echo "default"
+rustup default
 pnpm install
